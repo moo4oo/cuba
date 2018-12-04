65530f666ed52a3c128a645b5af989a48e802984
Dima <zrin089@gmail.com>
2018-12-04 14:33:59 +0500

    4.12.2018 updates

--- modules/core/src/com/company/example/core/ApprovalHelper.java
+++ modules/core/src/com/company/example/core/ApprovalHelper.java
@@ -2,6 +2,10 @@ package com.company.example.core;
 
 import com.company.example.entity.DocState;
 import com.company.example.entity.OutgoingDocuments;
+import com.haulmont.bpm.entity.ProcActor;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.core.global.View;
 import org.springframework.stereotype.Component;
 import com.haulmont.cuba.core.Persistence;
 import com.haulmont.cuba.core.Transaction;
@@ -9,6 +13,7 @@ import com.haulmont.cuba.core.Transaction;
 
 import javax.inject.Inject;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Component(ApprovalHelper.NAME)
@@ -16,7 +21,8 @@ public class ApprovalHelper {
     public static final String NAME = "demo_ApprovalHelper";
     @Inject
     private Persistence persistence;
-
+    @Inject
+    private DataManager dataManager;
 
     public void updateState(UUID entityId, String state) {
         DocState docState = DocState.fromId(state);
@@ -27,10 +33,32 @@ public class ApprovalHelper {
                 if(state.equals("Registered")){
                     doc.setDate(new Date());
                 }
+                tx.commit();
             }
-            tx.commit();
+
         }
     }
+
+    public boolean roleExists(String roleCode, UUID bpmProcInstanceId) {
+        LoadContext<ProcActor> ctx = LoadContext.create(ProcActor.class).setView(View.MINIMAL);
+        ctx.setQueryString("select pa from bpm$ProcActor pa where pa.procRole.code = :roleCode and " +
+                "pa.procInstance.id = :bpmProcInstanceId")
+                .setParameter("roleCode", roleCode)
+                .setParameter("bpmProcInstanceId", bpmProcInstanceId);
+        List<ProcActor> procActors = dataManager.loadList(ctx);
+        boolean result = false;
+        if(procActors.isEmpty())
+            result = true;
+        for(ProcActor actor : procActors){
+            if(actor.getUser() == null){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+
+
 
 
 }
