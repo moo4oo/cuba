8568b6d332e8f7e6e8f7519a828019f792eeee6a
Dima Zorin <zorin@finexcons.ru>
2018-12-11 16:03:24 +0500

    klkl

--- modules/core/src/com/company/example/service/EditWorkersServiceBean.java
+++ modules/core/src/com/company/example/service/EditWorkersServiceBean.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @Service(EditWorkersService.NAME)
 public class EditWorkersServiceBean implements EditWorkersService {
+
     
     @Override
     public Map<String, String> setFirstLastNames(User user) {
